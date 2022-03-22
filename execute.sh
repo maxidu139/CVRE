fae5d2729c559063df4b06bcb26ef0b61648a351
herreraa17 <99055237+herreraa17@users.noreply.github.com>
2022-03-21 23:52:47 -0300

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./Examples/Monocular-Inertial/mono_inertial_tum_vi Vocabulary/ORBvoc.txt Examples/Monocular-Inertial/TUM_512.yaml {pathToDataset}/dataset-corridor1_512_16/mav0/cam0/data Examples/Monocular-Inertial/TUM_TimeStamps/dataset-corridor1_512.txt Examples/Monocular-Inertial/TUM_IMU/dataset-corridor1_512.txt dataset-corridor1_512_monoi
